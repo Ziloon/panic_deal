b8dc83003dd8eda47b5ec4ecc999ab79be1705a0
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-01 00:05:09 +0800

    显式调用 panic! 宏

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
+    // 在实践中有两种方法造成 panic：执行会造成代码 panic 的操作（比如访问超过数组结尾的内容）或者显式调用 panic! 宏。这两种情况都会使程序 panic。通常情况下这些 panic 会打印出一个错误信息，展开并清理栈数据，然后退出。
+    panic!("crash and burn");
 }
