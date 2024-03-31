2cbdf6a75a265d4ae6d987cd47f28521fcbf64c7
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-01 00:35:08 +0800

    使用不同的方式处理不同类型的错误

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{f32::consts::E, fs::File, io::ErrorKind};
 
 fn main() {
     // 在实践中有两种方法造成 panic：执行会造成代码 panic 的操作（比如访问超过数组结尾的内容）或者显式调用 panic! 宏。这两种情况都会使程序 panic。通常情况下这些 panic 会打印出一个错误信息，展开并清理栈数据，然后退出。
@@ -11,6 +11,12 @@ fn main() {
     let greeting_file_result = File::open(&greeting_file_name);
     let greeting_file = match greeting_file_result{
         Ok(file) => file,
-        Err(error) => panic!("failly opening the '{}': {:?}", &greeting_file_name, error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(&greeting_file_name) {
+                Ok(fc) => fc,
+                Err(e) => panic!("fail creating the file '{}': {:#?}", &greeting_file_name, e),
+            },
+            other_error => panic!("fail opening file '{}': {:#?}", &greeting_file_name, other_error),
+        } 
     };
 }
