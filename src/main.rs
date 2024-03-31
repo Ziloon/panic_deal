462ce4e85ad9c383cdc44082e83087467c6dcd8d
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-01 00:18:35 +0800

    访问溢出引起的panic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     // 在实践中有两种方法造成 panic：执行会造成代码 panic 的操作（比如访问超过数组结尾的内容）或者显式调用 panic! 宏。这两种情况都会使程序 panic。通常情况下这些 panic 会打印出一个错误信息，展开并清理栈数据，然后退出。
-    panic!("crash and burn");
+    // panic!("crash and burn");
+
+    let v = vec![1, 2, 3];
+    
+    v[99];
 }
